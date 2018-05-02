b13b1821626064836fa7dfaa098bc00d39167ca9
Jared Jones <jkjones.dev@gmail.com>
2018-05-02 18:00:08 -0500

    Update runpth.sh

--- runpth.sh
+++ runpth.sh
@@ -1,4 +1,4 @@
-x-terminal-emulator msfrpcd -P password -n -f -a 127.0.0.1
+x-terminal-emulator -e msfrpcd -P password -n -f -a 127.0.0.1
 read -p 'Your local IP: ' localipvar
 read -p 'Target IP: ' targetipvar
 sudo python2.7 pth.py -l $localipvar -t $targetipvar
